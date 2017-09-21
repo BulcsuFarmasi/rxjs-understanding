3fb1ed5c665b97bfa44dc70faa536b3dc762f56c
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-09-21 21:47:29 +0200

    throttleTime operator

--- what-why/script.js
+++ what-why/script.js
@@ -1,6 +1,7 @@
 var button = document.querySelector('button');
 //button.addEventListener('click', event => {console.log(event)});
 Rx.Observable.fromEvent(button, 'click')
+    .throttleTime(1000)
     .subscribe(
-        event => {console.log(event)}
+        event => {console.log(event.clientX)}
     );
