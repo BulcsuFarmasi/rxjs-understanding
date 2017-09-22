cdeb9147e07d21e79ea059d7b3770c86f7ced0ee
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-09-22 18:09:14 +0200

    filter operator

--- filter/script.js
+++ filter/script.js
@@ -1,7 +1,9 @@
 var observable = Rx.Observable.interval(1000);
 
 observable
-    .filter()
+    .filter(function (value) {
+        return value % 2 == 0;
+    })
     .subscribe({
     next: function (value) {
         console.log(value)
