c4003f10240426679155a0f08b50148e8789f1c9
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-09-22 20:14:25 +0200

    nested subscriptions

--- switch-map/script.js
+++ switch-map/script.js
@@ -1,14 +1,10 @@
-var input1 = document.querySelector('#input1');
-var input2 = document.querySelector('#input2');
-var span = document.querySelector('span');
+var button = document.querySelector('button');
 
-var obs1 = Rx.Observable.fromEvent(input1, 'input');
-var obs2 = Rx.Observable.fromEvent(input2, 'input');
+var obs1 = Rx.Observable.fromEvent(button, 'click');
+var obs2 = Rx.Observable.interval(1000);
 
-obs1.mergeMap(
-    event1 => {
-        return obs2.map(event2 => event1.target.value + ' ' + event2.target.value)
-    }
-).subscribe(
-    combinedValue => { span.textContent = combinedValue }
+obs1.subscribe(
+    event => obs2.subscribe(
+        value => console.log(value)
+    )
 )
