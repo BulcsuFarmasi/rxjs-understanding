d88dbe3325493542bc202513e5e52a7a3fd762ee
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-09-22 16:30:49 +0200

    map & throttleTime operators

--- operators/script.js
+++ operators/script.js
@@ -1,30 +1,11 @@
-var button = document.querySelector('button');
-
+var observable = Rx.Observable.interval(1000);
 var observer = {
     next: function (value) {
-        console.log(value)
-    },
-    error: function (error) {
-        console.log(error)
-    },
-    complete: function () {
-        console.log('Completed!')
+        console.log(value);
     }
 }
-//Rx.Observable.fromEvent(button, 'click')
 
-var subscription = Rx.Observable.create(function (obs) {
-    //obs.next('A value');
-    //obs.error('Error');
-    //setTimeout(function () {
-    //    obs.complete();
-    //    obs.next('Another value');
-    //},2000)
-    button.addEventListener('click', function (event) {
-        obs.next(event);
-    })
-}).subscribe(observer);
-
-setTimeout(function () {
-    subscription.unsubscribe()
-}, 5000)
+observable.map(function (value) {
+    return 'Number: ' + value;
+}).throttleTime(2000)
+.subscribe(observer);
