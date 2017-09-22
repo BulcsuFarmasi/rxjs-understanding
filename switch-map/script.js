04e6905510205a16fbc459663a8b7576dcf7056f
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-09-22 20:23:30 +0200

    change title

--- switch-map/script.js
+++ switch-map/script.js
@@ -3,8 +3,10 @@ var button = document.querySelector('button');
 var obs1 = Rx.Observable.fromEvent(button, 'click');
 var obs2 = Rx.Observable.interval(1000);
 
-obs1.subscribe(
-    event => obs2.subscribe(
-        value => console.log(value)
-    )
+obs1.switchMap(
+    event => {
+        return obs2;
+    }
+).subscribe(
+    value => { console.log(value) }
 )
