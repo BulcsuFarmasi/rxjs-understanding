2d28048a4159e34ec0838efad9bb3483c261aa29
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-09-22 20:31:32 +0200

    Subject

--- behaviour-subject/script.js
+++ behaviour-subject/script.js
@@ -1,14 +1,11 @@
-var input1 = document.querySelector('#input1');
-var input2 = document.querySelector('#input2');
-var span = document.querySelector('span');
+var clickEmitted = new Rx.Subject();
+var button = document.querySelector('button');
+var div = document.querySelector('div');
 
-var obs1 = Rx.Observable.fromEvent(input1, 'input');
-var obs2 = Rx.Observable.fromEvent(input2, 'input');
+button.addEventListener('click', () => {
+    clickEmitted.next('Clicked!');
+});
 
-obs1.mergeMap(
-    event1 => {
-        return obs2.map(event2 => event1.target.value + ' ' + event2.target.value)
-    }
-).subscribe(
-    combinedValue => { span.textContent = combinedValue }
+clickEmitted.subscribe(
+    value => div.textContent =  value
 )
