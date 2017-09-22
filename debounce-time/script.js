f66bdb2d91d15fbaec759c4c920ab5eeea7f941b
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-09-22 18:20:10 +0200

    debounceTime operator

--- debounce-time/script.js
+++ debounce-time/script.js
@@ -2,6 +2,7 @@ var input = document.querySelector('input')
 var observable = Rx.Observable.fromEvent(input, 'input');
 
 observable
+    .debounceTime(500)
     .subscribe({
     next: function (event) {
         console.log(event.target.value)
