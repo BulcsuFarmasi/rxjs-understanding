7fbc0b8f8a69f623bc1f57296987587fb444a8e6
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-09-22 15:44:30 +0200

    simple subscription

--- observables/script.js
+++ observables/script.js
@@ -1,8 +1,5 @@
 var button = document.querySelector('button');
-//button.addEventListener('click', event => {console.log(event)});
 Rx.Observable.fromEvent(button, 'click')
-    .throttleTime(1000)
-    .map(data => data.clientY)
     .subscribe(
-        cooardinate => {console.log(cooardinate)}
+        event => {console.log(event.clientX)}
     );
